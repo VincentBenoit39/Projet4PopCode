26ecaee633ce9905ea49762f115e7d52b4b60f5f
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-06-28 11:06:25 +0200

    start

--- main/JS/main.js
+++ main/JS/main.js
@@ -1,1 +1,10 @@
+const loading = document.querySelectorAll(".loader") ;
+const content = document.getElementById("displayContent") ;
 
+window.load = setTimeout(showContent, 2000);
+
+function showContent() {
+  loading.style.display = "none";
+  loading.style.background = "none";
+  content.style.display = "block";
+}
