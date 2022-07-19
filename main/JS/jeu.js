fef63f79b8ab77a35a83b1c567576abe14c39f4a
VincentBenoit39 <102597078+VincentBenoit39@users.noreply.github.com>
2022-07-19 21:35:59 +0200

    st

--- main/JS/jeu.js
+++ main/JS/jeu.js
@@ -21,6 +21,7 @@ let score = document.querySelector(".check");
 let error1 = false
 let error2 = false
 let error3 = false
+let check = 0;
 
 window.addEventListener("keydown", function (e) {
     if (
